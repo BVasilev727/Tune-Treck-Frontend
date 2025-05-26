dccf68c41c6f4cd7f877a40dcce8f63269ad614f
BVasilev727 <frilans569@gmail.com>
2025-05-26 15:07:17 +0300

    centering of dashboard

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App()
   return (
     <>
     <Router>
-    <div className='container'>
+    <div className='w-full'>
       <Header />
       <Routes>
         <Route path='/' element={<GameComponent />} />
